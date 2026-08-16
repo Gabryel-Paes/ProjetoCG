dbdb5f37aec84307ba1fa16e0be544338f6050be
Nathan Edhuardo <nathantrabson@gmail.com>
2026-08-16 11:51:41 -0300

    feat: adiciona codigo inicial de inimigo

---
+++ levels/rooms/enemy___esqueleto.gd
@@ -0,0 +1,57 @@
+extends CharacterBody2D
+
+# Configurações
+@export var speed: float = 20.0
+
+enum PatrolAxis {
+	HORIZONTAL,
+	VERTICAL
+}
+
+@export var patrol_axis: PatrolAxis = PatrolAxis.HORIZONTAL
+
+var direction: Vector2
+var player: CharacterBody2D = null
+
+# Inicialização
+func _ready():
+
+	if patrol_axis == PatrolAxis.HORIZONTAL:
+		direction = Vector2.RIGHT
+	else:
+		direction = Vector2.DOWN
+
+# Loop principal
+func _physics_process(delta):
+
+	if player != null:
+
+		# Persegue o jogador
+		var chase_direction = (player.global_position - global_position).normalized()
+		velocity = chase_direction * speed
+
+	else:
+
+		# Patrulha
+		velocity = direction * speed
+
+	move_and_slide()
+
+	# Se estiver patrulhando e bater em algo,
+	# inverte a direção.
+
+	if player == null:
+
+		if get_slide_collision_count() > 0:
+			direction *= -1
+
+# Área de detecção
+func _on_detection_area_body_entered(body):
+
+	if body.is_in_group("Player"):
+		player = body
+
+func _on_detection_area_body_exited(body):
+
+	if body == player:
+		player = null
